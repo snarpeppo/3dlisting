4898521c16950fef851359c389aac9150dc90889
Gabriele <gabriele-battistata@it-present.com>
2022-03-25 12:20:00 +0100

    primevue table for submissions

--- src/components/submissions/SubmissionList.vue
+++ src/components/submissions/SubmissionList.vue
@@ -1,26 +1,19 @@
 <template>
   <div>
     <h2 class="mb-3">3D Queue</h2>
-    <table class="table table-hover" id="formtable" v-if="getQueue">
-      <thead class="table-primary">
-        <tr>
-          <th scope="col">Username</th>
-          <th scope="col">File</th>
-          <th scope="col">Filamento</th>
-          <th scope="col">timestamp</th>
-        </tr>
-      </thead>
-      <tbody>
-        <submission-item
-          v-for="submission in getQueue"
-          :key="submission.id"
-          :username="submission.username"
-          :fileName="submission.fileName"
-          :filament="submission.filamentColor"
-          :timestamp="submission.timestamp"
-        ></submission-item>
-      </tbody>
-    </table>
+    <DataTable
+      responsive-layout="scroll"
+      :value="subs"
+      v-if="getQueue.length > 0"
+    >
+      <Column
+        v-for="col of columns"
+        :field="col.field"
+        :header="col.header"
+        :key="col.field"
+        :sortable="true"
+      ></Column>
+    </DataTable>
     <div v-else>
       <p>non ci sono submissions, sii il primo a farne una!</p>
       <router-link to="/Submission">Clicca qui</router-link>
@@ -29,15 +22,28 @@
 </template>
 
 <script>
-import SubmissionItem from "../../components/submissions/SubmissionItem.vue";
 export default {
-  components: {
-    SubmissionItem,
+  data() {
+    return {
+      columns: null,
+      subs: null,
+    };
+  },
+
+  beforeUpdate() {
+    this.subs = this.getQueue;
   },
 
   created() {
     this.loadQueue();
+    this.columns = [
+      { field: "username", header: "Username" },
+      { field: "fileName", header: "File" },
+      { field: "filamentColor", header: "Filament" },
+      { field: "timestamp", header: "Timestamp" },
+    ];
   },
+
   computed: {
     getQueue() {
       return this.$store.getters["submissions/getQueue"];

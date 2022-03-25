7caa310627f1720bfce94c14f78fb67dd988bda8
Gabriele <gabriele-battistata@it-present.com>
2022-03-25 16:54:04 +0100

    css minor

--- src/components/base/BaseBigCard.vue
+++ src/components/base/BaseBigCard.vue
@@ -6,12 +6,13 @@
 
 <style scoped>
 .card {
-  border-radius: 12px;
+  border-radius: 8px;
   /*box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);*/
-  box-shadow: 3px 3px rgba(0, 0, 0, 0.26), -0.5em 0 0.4em rgba(0, 0, 0, 0.26);
+  box-shadow: 20px 8px 20px 0px rgb(0 0 0 / 26%),
+    -0.5em 0 0.4em rgb(0 0 0 / 26%);
   padding: 1rem;
   margin: 2rem auto;
   max-height: 40rem;
-  max-width: 55rem;
+  max-width: 80%;
 }
 </style>

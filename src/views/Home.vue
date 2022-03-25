0526ac8d53850777660304f54e6ec5a8401cb744
Gabriele <gabriele-battistata@it-present.com>
2022-03-25 16:54:24 +0100

    minor homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <HelloWorld />
+    <HomePage />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HomePage from "@/components/Homepage.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    HomePage,
   },
 };
 </script>
